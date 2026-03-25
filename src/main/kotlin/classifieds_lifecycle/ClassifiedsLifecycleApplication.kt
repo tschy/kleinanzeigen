67debc0b8ffdc15349efb3b95b0f72cd9a17116b
tschy <tamara.schymura@posteo.net>
2026-03-25 12:15:40 +0100

    Add pagination

--- src/main/kotlin/classifieds_lifecycle/ClassifiedsLifecycleApplication.kt
+++ src/main/kotlin/classifieds_lifecycle/ClassifiedsLifecycleApplication.kt
@@ -1,18 +1,27 @@
 package classifieds_lifecycle
 
+import org.jsoup.Jsoup
 
 fun main(args: Array<String>) {
-	val itemExtractor = ItemExtractor()
-	val fetcherService = FetcherService()
+    val itemExtractor = ItemExtractor()
+    val fetcherService = FetcherService()
 
-	//val url = "https://www.kleinanzeigen.de/s-fahrraeder/herren/12309/seite:2/rennrad/k0c217l3411r10+fahrraeder.art_s:herren"
-	url =
+    var n = 1
 
+    do {
+        val url = "https://www.kleinanzeigen.de/s-fahrraeder/herren/12309/seite:${n}/rennrad/k0c217l3411r10+fahrraeder.art_s:herren"
 
-	val url = File("src/test/resources/rennraeder20260319.htm")
-            .readText()
+        // get HTML content for this page
+        val body = fetcherService.fetch(url)
+        val soup = Jsoup.parse(body)
 
-	val items = itemExtractor.extract(fetcherService.fetch(url))
-
-	items.forEach {println(it)}
+        // test if pagination-next marker is present
+        if (soup.select(".pagination-next").isNotEmpty()) {
+            n += 1
+            val items = itemExtractor.extract(body)
+            items.forEach { println(it) }
+            Thread.sleep(1000)
+        }
+    }
+        while (soup.select(".pagination-next").isNotEmpty());
 }
